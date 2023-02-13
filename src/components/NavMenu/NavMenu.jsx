26ca9270942a565d76059fb58dd4a71336e1fa26
Sarah-Katz <Sarahkatz59@gmail.com>
2023-02-13 11:55:34 +0100

    Change buttons to Link

--- src/components/NavMenu/NavMenu.jsx
+++ src/components/NavMenu/NavMenu.jsx
@@ -1,16 +1,17 @@
 import { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { RxCross1 } from 'react-icons/rx';
 import LoginButton from '../LoginButton/loginButton';
 import { UserContext } from '../../context/UserContext';
 
 export default function NavMenu() {
     const {isLogged, logout} = useContext(UserContext)
-    
+
     return (
         <nav>
             <RxCross1 className='navmenu-cross' />
-            <button className='navmenu-button'>Jouer</button>
-            <button className='navmenu-button'>Profil</button>
+            <Link to='/categories' className='navmenu-link'>Jouer</Link>
+            <Link to='/profile' className='navmenu-link'>Profil</Link>
             {isLogged === true ? (
                 <button className='navmenu-button' onClick={() => logout()}>
                     Déconnexion
