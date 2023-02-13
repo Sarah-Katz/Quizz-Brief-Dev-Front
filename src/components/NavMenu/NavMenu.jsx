41a86bb51228ad1232a46bca130b01a3b4557378
ncilliez <ncilliez@gmail.com>
2023-02-13 10:29:39 +0100

    rootage 2

--- src/components/NavMenu/NavMenu.jsx
+++ src/components/NavMenu/NavMenu.jsx
@@ -1,5 +1,28 @@
-export default function NavMenu() {
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
     return (
-        <nav></nav>
-    );
+        <>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/Categorie">Catégorie</Link>
+                    </li>
+                    <li>
+                        <Link to="/Profile">Profile</Link>
+                    </li>
+                    <li>
+                        <Link to="/Quizz">Quizz</Link>
+                    </li>
+                    <li>
+                        <Link to="/Results">Résultats</Link>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    )
 }
+export default Navbar
