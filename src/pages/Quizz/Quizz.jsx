c0a73d9643ca1c160b3da50ec455ce785668eaa3
ncilliez <ncilliez@gmail.com>
2023-02-14 17:00:07 +0100

    avancement quizz

--- src/pages/Quizz/Quizz.jsx
+++ src/pages/Quizz/Quizz.jsx
@@ -1,107 +1,3 @@
-// import React, { useState, useEffect } from "react";
-
-// const url = "http://localhost:8000/api/questions";
-
-// export default function Quizz() {
-//   const [tableau, setTableau] = useState([]);
-//   const [isLoading, setIsLoading] = useState(false);
-//   const [error, setError] = useState(null);
-
-//   useEffect(() => {
-//     setIsLoading(true);
-//     fetch(url)
-//       .then(response => response.json())
-//       .then(data => {
-//         setTableau(data);
-//         setIsLoading(false);
-//       })
-//       .catch(error => {
-//         setError(error);
-//         setIsLoading(false);
-//       });
-//   }, []);
-
-//   if (isLoading) {
-//     return <div>Loading...</div>;
-//   }
-
-//   if (error) {
-//     return <div>An error occured: {error.message}</div>;
-//   }
-
-//   if (!tableau.length) {
-//     return <div>No data available</div>;
-//   }
-
-//   return (
-//     <div>
-//       <p>{tableau[0].question}</p>
-//       <p>truc</p>
-//     </div>
-//   );
-// }
-
-
-
-// import React, { useState, useEffect } from "react";
-
-// const url = "http://localhost:8000/api/questions";
-
-// export default function Quizz() {
-//   const [tableau, setTableau] = useState([]);
-//   const [isLoading, setIsLoading] = useState(false);
-//   const [error, setError] = useState(null);
-//   const [selection, setSelection] = useState([]);
-
-//   useEffect(() => {
-//     setIsLoading(true);
-//     fetch(url)
-//       .then(response => response.json())
-//       .then(data => {
-//         setTableau(data);
-//         let total = 0;
-//         let randomQuestions = [];
-
-//         while (total < 10 && data.length > 0) {
-//           let random = rand(data.length);
-//           randomQuestions.push(data[random]);
-//           data.splice(random, 1);
-//           total++;
-//         }
-//         setSelection(randomQuestions);
-//         setIsLoading(false);
-//       })
-//       .catch(error => {
-//         setError(error);
-//         setIsLoading(false);
-//       });
-//   }, []);
-
-//   if (isLoading) {
-//     return <div>Loading...</div>;
-//   }
-
-//   if (error) {
-//     return <div>An error occured: {error.message}</div>;
-//   }
-
-//   if (!tableau.length) {
-//     return <div>No data available</div>;
-//   }
-
-//   function rand(max) {
-//     return Math.floor(Math.random() * max);
-//   }
-
-//   return (
-//     <div>
-//       {selection.map((item, i) => (
-//         <div key={i}>{item.question}</div>
-//       ))}
-//     </div>
-//   );
-// }
-
 import React, { useState, useEffect } from "react";
 
 const url = "http://localhost:8000/api/questions";
@@ -111,6 +7,7 @@ export default function Quizz() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [selection, setSelection] = useState([]);
+  const [question, setQuestion]= useState(0)
 
   useEffect(() => {
     setIsLoading(true);
@@ -124,12 +21,47 @@ export default function Quizz() {
 
         while (total < 10 && data.length > 0) {
           let random = rand(data.length);
-          randQuestions.push(data[random]);
+          let t = 0
+          let selectionReponse= []
+          let rep =[
+            data[random].reponse1,
+            data[random].reponse2,
+            data[random].reponse3,
+            data[random].reponse4,
+            data[random].reponse5,
+            data[random].reponse6,
+            data[random].reponse7,
+            data[random].reponse8,
+            data[random].reponse9,
+            data[random].reponse10 
+          ]
+
+          
+          while(t < 4)
+          {
+            let randomRep = rand(rep.length);
+            selectionReponse.push(rep[randomRep])
+            rep.splice(randomRep, 1);
+            t++;
+          }
+
+         
+
+          let donner  = {
+            question: data[random].question,
+            categorie: data[random].categorie,
+            rep1: selectionReponse[0],
+            rep2: selectionReponse[1],
+            rep3: selectionReponse[2],
+            rep4: selectionReponse[3]
+        }
+
+          randQuestions.push(donner);
           data.splice(random, 1);
-          total++;
+          total++;  
+         
         }
         setSelection(randQuestions);
-
         setIsLoading(false);
       })
       .catch(error => {
@@ -138,7 +70,6 @@ export default function Quizz() {
       });
   }, []);
 
-  console.log(selection)
 
   function rand(max) {
     return Math.floor(Math.random() * max);
@@ -156,11 +87,25 @@ export default function Quizz() {
     return <div>No data available</div>;
   }
 
+  const next = () =>{
+
+        setQuestion(question+1);
+  
+  };
+
+
+
   return (
     <div>
       
         <div >
-          <p>{selection[0].question}</p>
+          <p>{selection[question].question}</p>
+            <ul>
+              <li onClick = {next}>{selection[question].rep1}</li>
+              <li onClick = {next}>{selection[question].rep2}</li>
+              <li onClick = {next}>{selection[question].rep3}</li>
+              <li onClick = {next}>{selection[question].rep4}</li>
+            </ul>
         </div>
    
     </div>
