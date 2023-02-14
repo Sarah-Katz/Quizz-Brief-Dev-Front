9639b81b9aa3066f2767d640abe3dbbada49445a
Abdallah Taklit <a.taklit@icloud.com>
2023-02-14 17:02:00 +0100

    Creation login

--- src/pages/Quizz/Quizz.jsx
+++ src/pages/Quizz/Quizz.jsx
@@ -1,9 +1,11 @@
+import LoginRegister from '../../components/LoginRegister/LoginRegister';
 import NavMenu from '../../components/NavMenu/NavMenu'
 
 export default function Quizz() {
     return (
         <div>
             <NavMenu />
+            <LoginRegister/>
         </div>
     );
 }
