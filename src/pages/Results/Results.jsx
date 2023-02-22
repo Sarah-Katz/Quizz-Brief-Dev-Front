e868ffd1156efbf099e671ee31a2b388f05a8842
ncilliez <ncilliez@gmail.com>
2023-02-22 16:38:19 +0100

    cursor + page results

--- src/pages/Results/Results.jsx
+++ src/pages/Results/Results.jsx
@@ -1,14 +1,18 @@
 import LoginButton from '../../components/LoginButton/loginButton';
 // import NavMenu from '../../components/NavMenu/NavMenu'
 import { Link } from 'react-router-dom';
+import '../Results/Results.css'
 
 export default function Results({first}) {
     const isLogged = localStorage.getItem('isLogged');
     var paramUrl = window.location.href.split('http://localhost:3000/results?note=')
     return (
-        <div>
-            {/* <NavMenu /> */}
-            <div className='results-container'>{paramUrl}</div>
+        <div className='results-container'>
+
+            <div className='results-score'>
+                <div>Votre score:</div>
+                <div className='score'>{paramUrl}</div>
+            </div>
             <Link to='/categories'>Retour au catégories</Link>
             <LoginButton />
         </div>
